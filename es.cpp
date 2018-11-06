09da9807622588f6f9e14effd27737cd23d1eb60
capello <bcapello@ws18.labo7>
2018-11-05 21:47:55 -0300

    Carga de toroide

--- es.cpp
+++ es.cpp
@@ -1,10 +1,47 @@
 #include "es.h"
+#include <iostream>
+#include <fstream>
 #include "definiciones.h"
 
 /******************************** EJERCICIO cargarToroide *******************************/
 toroide cargarToroide(string nombreArchivo, bool &status)
 {
+
+	int cantidadFilas;
+	int cantidadColumnas;
 	toroide t;
+	ifstream fin;
+	fin.open(nombreArchivo, ifstream::in);
+
+	if (fin.fail()) {
+		status = false;
+		return t;
+	}
+
+	fin >> cantidadFilas;
+	fin >> cantidadColumnas;
+	int contadorVivas = 0;
+	for (int i = 0; i < cantidadFilas; ++i) {
+		t.push_back(vector<bool>(cantidadColumnas, false));
+		for (int j = 0; j < cantidadColumnas; ++j) {
+			int celda;
+			fin >> celda;
+			if (celda == 1) {
+				t[i][j] = true;
+				contadorVivas++;
+			} else if(celda == 0) {
+				t[i][j] = false;
+			} else {
+				status = false;
+				return t;
+			}
+		}
+	}
+	int cantidadVivas;
+	fin >> cantidadVivas;
+	status = (cantidadVivas == contadorVivas);
+	fin.close();
+
 	return t;
 }
 
