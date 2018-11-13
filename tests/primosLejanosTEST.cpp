409278a9c4045307e3b99bbe7a7a626d45c53256
a-lavia <lavia.alejandro@gmail.com>
2018-11-13 00:38:01 -0300

    Agregado tests de primosLejanos para completar el coverage

--- tests/primosLejanosTEST.cpp
+++ tests/primosLejanosTEST.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 TEST(primosLejanosTEST, toroideHorizontalPeriodico2){
-    toroide t1 = { 
-                  {false, false, false, false, false}, 
-                  {false, false, false, false, false}, 
+    toroide t1 = {
+                  {false, false, false, false, false},
+                  {false, false, false, false, false},
                   {false, true, true, true, false},
-                  {false, false, false, false, false}, 
+                  {false, false, false, false, false},
                   {false, false,false, false, false} };
-    toroide t2 = { 
-                  {false, false, false, false, false}, 
-                  {false, false, true, false, false}, 
+    toroide t2 = {
+                  {false, false, false, false, false},
                   {false, false, true, false, false},
-                  {false, false, true, false, false}, 
+                  {false, false, true, false, false},
+                  {false, false, true, false, false},
                   {false, false,false, false, false} };
     bool res = primosLejanos(t1, t2);
     EXPECT_TRUE(res);
@@ -24,17 +24,51 @@ TEST(primosLejanosTEST, toroideHorizontalPeriodico2){
 }
 
 TEST(primosLejanosTEST, diferentesDimensiones){
-    toroide t1 = { 
-                  {false, false, false, false, false}, 
-                  {false, false, false, false, false}, 
+    toroide t1 = {
+                  {false, false, false, false, false},
+                  {false, false, false, false, false},
                   {false, true, true, true, false},
-                  {false, false, false, false, false}, 
+                  {false, false, false, false, false},
                   {false, false,false, false, false} };
-    toroide t2 = { 
-                  {false, false, false, false, false}, 
-                  {false, false, true, false, false}, 
+    toroide t2 = {
+                  {false, false, false, false, false},
                   {false, false, true, false, false},
-                  {false, false, true, false, false}}; 
+                  {false, false, true, false, false},
+                  {false, false, true, false, false}};
+    bool res = primosLejanos(t1, t2);
+    EXPECT_FALSE(res);
+    res = primosLejanos(t2, t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(primosLejanosTEST, mismasDimensiones){
+    toroide t1 = {
+                  {false, false, false, false, false},
+                  {false, false, false, false, false},
+                  {false, true, true, true, false},
+                  {false, false, false, false, false},
+                  {false, false,false, false, false} };
+		toroide t2 = {
+                  {false, true, false, false, false},
+                  {false, false, false, false, false},
+                  {false, true, true, true, false},
+                  {false, false, false, false, false},
+                  {false, false,false, false, false} };
+    bool res = primosLejanos(t1, t2);
+    EXPECT_FALSE(res);
+    res = primosLejanos(t2, t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(primosLejanosTEST, unoEsPeriodico){
+    toroide t1 = {
+                  {true, true, false},
+                  {true, true, false},
+                  {false, false, false} };
+		toroide t2 = {
+	                {true, false, false},
+	                {false, true, false},
+	                {false, false, true} };
     bool res = primosLejanos(t1, t2);
     EXPECT_FALSE(res);
     res = primosLejanos(t2, t1);
